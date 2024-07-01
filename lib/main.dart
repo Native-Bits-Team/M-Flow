f3ec092b0953376438b7e0d156dec25921a05b69
Madhur Pandey <107851371+Madhur6@users.noreply.github.com>
2024-07-01 23:12:09 +0530

    Implemented Dynamic Line Manipulation with a very simple logic (Created a Custom md-syntax: 'm$') (#5)
    * Implemented Dynamic Line Manipulation with a very simple logic
    
    * fixed '\n' bug in getUserInput() method...
    
    ---------
    
    Conflicts fixed by: Imad Laggoune <135859673+imad23m@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:m_flow/pages/dashboard.dart';
 import 'package:m_flow/pages/form_page.dart';
-void main() => runApp(const MyApp());
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: FormPage(initText: ""),
+
+      // Theme for our entire app can be set from here...
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blueGrey,
+        ),
+      ),
     );
   }
 }
